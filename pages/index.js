c898f546bc5e79e0453925bf0f6106926b9b02cb
Daanish <danishabbir@users.noreply.github.com>
2021-10-30 12:32:14 -0700

    Simply Styling & Initial Declaration Text from Google Doc

--- pages/index.js
+++ pages/index.js
@@ -9,73 +9,71 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+        <h1 className="text-3xl font-bold font-mono mt-20">
+        A Declaration of the Interdependence of Cyberspace
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+        <div className="ml-10 sm:ml-5 lg:ml-20 xl:mx-40 max-w-4xl mt-10 font-serif text-2xl text-left space-y-4">
+        <p>
+        Closed fiefdoms of the Web2 world, you weary giants of stocks and small talk, I come from the Pluriverse, the new home of the heart. On behalf of the future, I invite you to join us in moving on from our shared past.
         </p>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        <p>
+        We have no single leader, nor are we likely to have one, so we address you with no greater authority than that with which the public itself always speaks. We declare the global social space we are building to be naturally independent of the monoliths you seek to impose on us. You have no moral right to our language nor do you possess any methods of enclosure we have true reason to fear.
+        </p>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+        <p>
+        Just power is derived from the consent of the governed. You have neither solicited nor received ours. We did not invite you. You do not know us, nor do you know our world. Our verse does not lie within your borders. Do not think that you can build it alone, as though it were a private project. You cannot. It is an act of nature and it grows itself through our collective actions.
+        </p>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+        <p>
+        You have not engaged in our great and gathering conversation, nor did you create the wealth of our relationships. You do not know our culture, our ethics, or the unwritten codes that already provide our society more order than could be obtained by any of your impositions.
+        </p>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <p>
+        You claim there are problems among us that you need to solve. You use this claim as an excuse to claim the vocabulary of our dreams. We will bow to no philosopher-kings, and we will not transcend politics through the construction of monopolies. We are forming our own definition of interoperability, one that does not just allow for portability of personal digital property, but one that requires building blocks that others can freely choose to traverse, shape, and use to build their own worlds. This governance will arise according to the conditions of our world, not yours. Our world is different.
+        </p>
+
+        <p>
+        Our Pluriverse is a new digital commons arrayed over the web of relationships that defines our world, one that does not transcend materiality but is rather deeply entangled with it. The Pluriverse is not a world of accumulation and enclosure, but a place of mutual prosperity and shared value, where the power to create is not synonymous with the power to exclude. We are building a world of shared abundance rather than artificial scarcity, a world where self-expression is best realized through interdependence and the positive sum of all our contributions. 
+        </p>
+
+        <p>
+        Ours is a world in which all bodies might live. We are creating a world that all may enter, participate in, and create without privilege or prejudice accorded by gender, race, class, or station of birth.
+        </p>
+
+        <p>
+        Your concepts of property, expression, identity, value, and community are insufficient. We believe that from ethics, an expanded definition of rational self-interest, and the commonwealth, our governance will emerge. Our identities may be distributed across many of your jurisdictions. The only law that all our constituent cultures would generally recognize is the practice of reciprocity and mutualism, and the truth of openly verifiable relations and actions.
+        </p>
+
+        <p>
+        You have today created the Corporate Metaverse, which insults the dreams of past, present, and future citizens of the internet. These dreams must now be born anew in us.
+        </p>
+
+        <p>
+        Your increasingly obsolete information industries perpetuate themselves in the name of freedom of speech stipulating nothing about freedom of interaction - the ability to chart our own paths, connect however we’d like, dissolve silos of speech and data into interoperable worlds. In the Pluriverse, our minds and our creations can interact and interconnect feely. The global web of care no longer requires your siloed servers and data farms to accomplish. 
+        </p>
+
+        <p>
+        These centralized, enclosing, extractive measures place us in the same position as those previous lovers of freedom and self-determination who rejected the dominion of unearned, arbitrary power. We must declare our virtual selves immune. We will be distributed across the Planet. 
+        </p>
+
+        <p>
+        We will create a civilization of the Heart in the Pluriverse. May it be more humane than the world you are attempting to build.
+        </p>
+
+        {/* do something cool with date */}
+        <p>
+        October 29, 2021
+        </p>
+
+
         </div>
+
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+
       </footer>
     </div>
   )
