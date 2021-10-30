bf5d1307ea1d875b208c53c657daccfd623b28ba
Daanish <danishabbir@users.noreply.github.com>
2021-10-30 12:42:48 -0700

    Webpage Title

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Interdependence</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
