d71e6200ff7ba54e06723d15c55bc27399df8971
Daanish <danishabbir@users.noreply.github.com>
2021-10-31 20:49:15 -0700

    fork btn on about page

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Fork from "../components/Fork";
 
-export default function About() {
+export default function About({ data, sigs, txId, status }) {
 
 return (
     <Fork text={data} txId={txId} walletKey={""} />
