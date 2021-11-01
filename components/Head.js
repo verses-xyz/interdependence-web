e0a8a2e852cac59f930c63e9bc51ee360750097c
Daanish <danishabbir@users.noreply.github.com>
2021-11-01 14:06:42 -0700

    metadata tags

--- components/Head.js
+++ components/Head.js
@@ -5,5 +5,14 @@ export default function HeadComponent() {
     <title>Interdependence</title>
     <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,600;1,400&family=Roboto+Mono&display=swap" rel="stylesheet"/>
     <link rel="icon" href="/favicon.jpeg" />
+    
+    <meta
+          property="og:description"
+          content="Declaration of the Interdependence of Cyberspace"
+        />
+    
+    <meta property="og:image" content={"/public/og-image.png"} />
+    
+
   </Head>
 }
