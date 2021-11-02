8f4e2e7e21e4fc7dae41c56af73706bfbeb55b82
Nicholas Inzucchi <ninzucchi@gmail.com>
2021-11-02 16:24:19 -0700

    reverted to gray wash card headers

--- components/core/Box.js
+++ components/core/Box.js
@@ -1,7 +1,7 @@
 export default function Box({ title, content }) {
   return (
     <div className="border border-gray-detail rounded-md overflow-hidden">
-      <h2 className="font-mono py-3 bg-gray-primary text-gray-wash font-bold">{title}</h2>
+      <h2 className="font-mono py-3 bg-gray-wash text-gray-primary border-b border-gray-detail font-bold">{title}</h2>
       <div className="px-4 py-2 md:px-8 text-gray-primary">{content}</div>
     </div> 
   );
