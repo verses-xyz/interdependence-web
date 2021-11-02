89af305767bc881809bddbb97915c1e347c32569
Raymond Zhong <raykyri@users.noreply.github.com>
2021-11-01 18:41:57 -0700

    Minor styling (#31)

--- components/Sign.js
+++ components/Sign.js
@@ -72,7 +72,7 @@ export default function Sign({ txId, walletKey }) {
                 <input className="font-mono rounded-b-lg border-b-2 border-l-2 border-r-2 border-gray-200 focus:outline-none w-full px-4 py-4" type="text"{...register("handle")} autocomplete="off" placeholder="Your Twitter handle (optional)"/>
               </div>
               <div className="mt-2 text-center">
-                <Button primary>Submit</Button>
+                <Button className="mt-5 px-6 py-2 rounded-full bg-truegray-800 hover:text-gray-100 text-white text-sm sm:text-base font-mono" primary>Sign with Metamask</Button>
               </div>
             </div>
           </form>
