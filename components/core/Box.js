6bbe9e3dc2568f8afdb71e310b29e5b5970104d3
Nicholas Inzucchi <ninzucchi@gmail.com>
2021-11-02 15:56:06 -0700

    empty commit to try and get Vercel to recognize my account

--- components/core/Box.js
+++ components/core/Box.js
@@ -3,6 +3,6 @@ export default function Box({ title, content }) {
     <div className="border border-gray-detail rounded-md overflow-hidden">
       <h2 className="font-mono py-3 bg-gray-primary text-gray-wash font-bold">{title}</h2>
       <div className="px-4 py-2 md:px-8 text-gray-primary">{content}</div>
-    </div>
+    </div> 
   );
 };
