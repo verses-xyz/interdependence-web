dacc2ad02e694df43b3e7120d4ea4a92aa316ea3
Daanish <danishabbir@users.noreply.github.com>
2021-11-02 21:12:20 -0700

    softlaunch footer styling

--- pages/declaration/[txId].js
+++ pages/declaration/[txId].js
@@ -112,7 +112,7 @@ export default function Declaration() {
       </main>
       
     </div>      
-    <footer className="bg-truegray w-full px-10 py-3 mt-2 mb-2 text-bold text-md font-mono text-center justify-center text-black"> 
+    <footer className="sticky bottom-0 bg-truegray-800 w-full px-10 py-3 mt-2 mb-2 text-bold text-md font-mono text-center justify-center text-white"> 
 
         <p>
         You were trusted to steward this link -
